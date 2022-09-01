e680abb4f2a4309b91dfb348e731d8e2143d5cce
Sofia Guimarães <marysoffis123@gmail.com>
2022-09-01 10:38:18 -0300

    conta feita

--- src/CincoUm/Teste.java
+++
@@ -1,9 +0,0 @@
-package CincoUm;
-
-public class Teste {
-    public static void main(String[] args) {
-        Conta teste = new Conta();
-
-        teste.sacar(100);
-    }   
-}
--- src/Conta.java
+++ src/Conta.java
@@ -1,37 +1,53 @@
-package CincoTres;
 
-public class GetAndSet {
 
-    private String titular;
-    private int numero;
+public class Conta {
+    // variaveis
+    private static int totalDeContas;
+    private String titular;    
     private double saldo;
+    private int numero;
     
+    // construtor
+    Conta(String titular) {
+        this.titular = titular;
+        Conta.totalDeContas++;
+    }
+
+    // metodos
     public void sacar(double valor) {
         if(this.saldo >= valor) {
-                this.saldo -= valor;
+            this.saldo -= valor;
         } else {
-                System.out.println("Sem saldo suficiente");
+            System.out.println("Sem saldo suficiente");
         }
     }
-//getandset para "titular"//
+    
+    // getters and setters
     public String getTitular() {
         return titular;
     }
+    
     public void setTitular(String titular) {
         this.titular = titular;
     }
-//getandset para "número"//
+    
     public int getNumero() {
         return numero;
     }
+
     public void setNumero(int numero) {
         this.numero = numero;
     }
-//getandset para "saldo"//
+    
     public double getSaldo() {
         return saldo;
     }
+    
     public void setSaldo(double saldo) {
         this.saldo = saldo;
+    }
+
+    public int getTotalDeConta(){
+        return Conta.totalDeContas;
     }
 }
